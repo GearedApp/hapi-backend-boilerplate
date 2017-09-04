1f70db1de073087579e7ac73c100d9673c30f0a7
German <germanhernandez.dr@gmail.com>
2017-09-04 10:19:21 +0100

    Updated gitignore, added basic configuration into server.js in order to run the server

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const Hapi = require('Hapi');
+
+// Create server connection using host and port
+const server = new Hapi.Server();
+server.connection({ host: 'localhost', port: 8000 });
+
+// Start the server
+server.start((err) => {
+  if (err) throw err;
+
+  console.log(`Server running at: ${server.info.uri}`);
+});
